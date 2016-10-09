dfdd2f670873f1bc1b769db2f0a88a1ec3e7eedb
Gil Barbara <gilbarbara@gmail.com>
2016-10-08 22:05:19 -0300

    update tests with Enzyme

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,7 @@
 /*eslint-disable no-console, no-underscore-dangle */
 import expect from 'expect';
 import React from 'react';
-import ReactDOM from 'react-dom/server';
-import TestUtils from 'react-addons-test-utils';
+import { mount } from 'enzyme';
 
 import ReactInlineSVG from '../src';
 
@@ -15,7 +14,7 @@ const svgResponse = { text: '<svg xmlns="http://www.w3.org/2000/svg" width="24" 
  * @returns {*}
  */
 function setup(props = {}) {
-  return TestUtils.renderIntoDocument(
+  return mount(
     <ReactInlineSVG {...props} />
   );
 }
@@ -45,7 +44,7 @@ describe('react-inlinesvg', () => {
 
       setup({
         src: 'https://raw.githubusercontent.com/google/material-design-icons/master/av/svg/production/ic_play_arrow_24px.svg',
-        onError: (err) => {
+        onError: err => {
           done(err);
         },
         onLoad: () => {
@@ -62,7 +61,7 @@ describe('react-inlinesvg', () => {
         onLoad: () => {
           setup({
             src: 'https://raw.githubusercontent.com/google/material-design-icons/master/av/svg/production/ic_play_arrow_24px.svg',
-            onError: (err) => {
+            onError: err => {
               done(err);
             },
             onLoad: () => {
@@ -81,12 +80,12 @@ describe('react-inlinesvg', () => {
   });
 
   it('should be a Component', () => {
-    const render = setup({ src: '' });
+    const wrapper = setup({ src: '' });
 
-    expect(TestUtils.isCompositeComponent(render)).toBe(true);
+    expect(wrapper.instance() instanceof React.Component).toBe(true);
   });
 
-  it('should load an svg', (done) => {
+  it('should load an svg', done => {
     setup({
       src: '/test/tiger.svg',
       onError: done,
@@ -96,7 +95,7 @@ describe('react-inlinesvg', () => {
     });
   });
 
-  it('should load a base64 data-uri', (done) => {
+  it('should load a base64 data-uri', done => {
     setup({
       src: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICAgIDxwYXRoIGQ9Ik04IDV2MTRsMTEtN3oiLz4KICAgIDxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz4KPC9zdmc+Cg==',
       onError: done,
@@ -106,7 +105,7 @@ describe('react-inlinesvg', () => {
     });
   });
 
-  it('should load a non-base64 data-uri', (done) => {
+  it('should load a non-base64 data-uri', done => {
     setup({
       src: 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%3E%0A%20%20%20%20%3Cpath%20d%3D%22M8%205v14l11-7z%22%2F%3E%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%0A%3C%2Fsvg%3E%0A',
       onError: done,
@@ -116,14 +115,14 @@ describe('react-inlinesvg', () => {
     });
   });
 
-  it('should call onError for a 404', (done) => {
+  it('should call onError for a 404', done => {
     setup({
       src: 'DOESNOTEXIST.svg',
       onError: () => done()
     });
   });
 
-  it('should load SVGs from a CORS-enabled domain', (done) => {
+  it('should load SVGs from a CORS-enabled domain', done => {
     setup({
       src: 'http://localhost:1338/test/tiger.svg',
       onError: done,
@@ -133,67 +132,48 @@ describe('react-inlinesvg', () => {
     });
   });
 
-  context('errors', () => {
-    it('should show children if loading not supported', (done) => {
+  context('with errors', () => {
+    it('should show children if loading not supported', () => {
       const props = {
         src: 'DOESNOTEXIST.svg',
         children: [React.DOM.span({ key: 1 }, ''), React.DOM.span({ key: 2 }, 'MISSINGNO')],
-        supportTest: () => false,
-        onError: () => {
-          const el = ReactDOM.renderToStaticMarkup(<ReactInlineSVG {...props} />);
-
-          if (/MISSINGNO/.test(el)) {
-            return done();
-          }
-
-          return done(new Error('Missing fallback contents'));
-        }
+        supportTest: () => false
       };
 
-      setup(props);
+      const wrapper = setup(props);
+      expect(wrapper.find('.isvg > span').length).toBe(2);
     });
 
-    it('should show a single children if loading not supported', (done) => {
+    it('should show a single children if loading not supported', () => {
       const props = {
         src: 'DOESNOTEXIST.svg',
         children: React.DOM.img({ src: '/test/tiger.png' }),
-        supportTest: () => false,
-        onError: () => {
-          const el = ReactDOM.renderToStaticMarkup(<ReactInlineSVG {...props} />);
-
-          if (/src="\/test\/tiger.png"/.test(el)) {
-            return done();
-          }
-
-          return done(new Error('Missing fallback contents'));
-        }
+        supportTest: () => false
       };
+
+      const wrapper = setup(props);
+      const img = wrapper.find('img');
+
+      expect(img.length).toBe(1);
+      expect(img.props().src).toBe('/test/tiger.png');
+    });
+
+    it('should NOT show children on error', () => {
+      const props = {
+        src: 'DOESNOTEXIST.svg',
+        children: [React.DOM.span({ key: 1 }, ''), React.DOM.span({ key: 2 }, 'MISSINGNO')]
+      };
+
+      const wrapper = setup(props);
+      expect(wrapper.find('.isvg').hasClass('loading')).toBe(true);
 
       setup(props);
     });
 
-    it('should should NOT show children on error', (done) => {
+    it('should have a status code HTTP errors', done => {
       const props = {
         src: 'DOESNOTEXIST.svg',
-        children: [React.DOM.span({ key: 1 }, ''), React.DOM.span({ key: 2 }, 'MISSINGNO')],
-        onError: () => {
-          const el = ReactDOM.renderToStaticMarkup(<ReactInlineSVG {...props} />);
-
-          if (/MISSINGNO/.test(el)) {
-            return done(new Error('Children shown even though loading is supported'));
-          }
-
-          return done();
-        }
-      };
-
-      setup(props);
-    });
-
-    it('should have a status code HTTP errors', (done) => {
-      const props = {
-        src: 'DOESNOTEXIST.svg',
-        onError: (err) => {
+        onError: err => {
           if (err.isHttpError && err.status === 404) {
             return done();
           }
