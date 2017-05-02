fd99734e69621b47079de591d644247e8942e9ee
Gil Barbara <gilbarbara@gmail.com>
2017-05-02 00:12:54 -0300

    update tests to jsdom 10.x

--- test/setup.js
+++ test/setup.js
@@ -1,24 +1,8 @@
-import jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>', { url: 'http://localhost:1337' });
-const win = doc.defaultView; // get the window object out of the document
+const { window } = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost:1337' });
 
-global.document = doc;
-global.window = win;
+global.document = window.document;
+global.window = window;
 global.navigator = { userAgent: 'node.js' };
-
-// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-function propagateToGlobal(window) {
-  for (const key in window) {
-    if (!window.hasOwnProperty(key)) {
-      continue;
-    }
-    if (key in global) {
-      continue;
-    }
-
-    global[key] = window[key];
-  }
-}
-
-propagateToGlobal(win);
+global.atob = window.atob;
