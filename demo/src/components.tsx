d34b0c6bacb76f0bb79477e6850056beac88a90f
Gil Barbara <gilbarbara@gmail.com>
2020-09-05 19:40:17 -0300

    Add demo for codesandbox

---
+++ demo/src/components.tsx
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components';
+
+interface IGrid {
+  size: number;
+}
+
+export const SubTitle = styled.h2`
+  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+  margin-top: 0;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: ${({ size = 3 }: IGrid) => css`repeat(${size}, 1fr)`};
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  justify-content: center;
+  margin: 50px auto 0;
+  max-width: 960px;
+
+  @media (min-width: 1024px) {
+    grid-column-gap: 30px;
+    grid-row-gap: 30px;
+  }
+
+  ${SubTitle} + & {
+    margin-top: 0;
+  }
+`;
+
+export const GridItem = styled.div`
+  h4 {
+    margin-top: 0;
+    margin-bottom: 20px;
+  }
+
+  > div {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin-top: auto;
+  }
+
+  svg {
+    height: auto;
+    width: 100%;
+  }
+`;
+
+export const Wrapper = styled.main`
+  font-family: sans-serif;
+  padding: 15px;
+  text-align: center;
+`;
