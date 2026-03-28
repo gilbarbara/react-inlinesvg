3a15b3ba75b3433e9452b9286f33ea56827c58fb
Gil Barbara <gilbarbara@gmail.com>
2026-03-28 12:13:50 -0300

    Add useInlineSVG hook to handle the lifecycle

--- src/modules/hooks.tsx
+++ src/modules/hooks.tsx
@@ -1,4 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { EffectCallback, useEffect, useRef } from 'react';
+
+export function useMount(effect: EffectCallback) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(effect, []);
+}
 
 export function usePrevious<T>(state: T): T | undefined {
   const ref = useRef<T>(undefined);
